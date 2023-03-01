2761ca93d5cf905dab45170084a9064a90f15c08
JNAMx03 <onik.1899@gmail.com>
2023-03-01 15:47:39 -0500

    carpeta tabs, page login o sign y load screen

---
+++ src/pages/LoadScreen.tsx
@@ -0,0 +1,1 @@
+import { IonPage } from "@ionic/react";
---
+++ src/pages/LogInOrSignUp.tsx
@@ -0,0 +1,11 @@
+import { IonPage } from "@ionic/react";
+
+const Home: React.FC = () => {
+    return (
+      <IonPage>
+        
+      </IonPage>
+    );
+  };
+  
+export default Home;
